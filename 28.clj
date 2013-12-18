13bd12127de6a0e25e457c3ce91eb9eb0f531934
Jeff Carpenter <gcarpenterv@gmail.com>
2013-12-17 20:10:13 -0800

    Solve 28

--- 28.clj
+++ 28.clj
@@ -1,14 +1,21 @@
 ; Problem 28
 ; Flatten a Sequence
-(fn myflat [xs]
-  (cond
-    (empty? xs)
-      []
-    (or (list? (first xs)) (vector? (first xs)))
-      (do 
-        (println (first xs) "list")
-        (conj (myflat (first xs)) (myflat (rest xs))))
-    :else
-      (do
-        (println xs "scalar")
-        xs)))
+
+; Had to cheat - peeked at Clojure's implementation of flatten
+; http://clojuredocs.org/clojure_core/1.2.0/clojure.core/flatten 
+
+(fn [xs]
+  (filter (complement sequential?)
+          (rest (tree-seq sequential? seq xs))))
+
+; compement
+  ; returns a function that returns the logical opposite of the original function given the same params
+
+; sequential?
+  ; is this a sequence?
+
+; tree-seq
+  ; does a DFS, returning all steps along the way 
+
+; seq
+  ; converts a collection into a sequence
